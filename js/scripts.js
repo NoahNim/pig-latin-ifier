b46b9cf49620f1f95b920819b367ccc68bdf1719
Noah Medoff and Randy Weyer <noah@>
2018-03-28 13:45:21 -0700

    create arrays from user input that have arrays inside them to store the user input for iterations

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,25 @@
 // Business Logic
 // Variable for constanants. Excludes q and y for the purposes of this program
-var consonants = [B, C, D, F, G, H, J, K, L, M, N, P, R, S, T, V, W, X, Z,];
+var consonants = ["B", "b", "C", "c", "D", "d", "F", "f", "G", "g", "H", "h", "J", "j", "K", "k", "L", "l", "M", "m", "N", "n", "P", "p", "R", "r", "S", "s", "T", "t", "V", "v", "W", "w", "X", "x", "Z", "z"];
 // Variable containing vowels
-var vowels = [A, E, I, O, U];
+var vowels = ["A", "a", "E", "e", "I", "i", "O", "o", "U", "u"];
+
+var wordArray = function(sentence){
+  var theArray = sentence.split(" ");
+  return theArray;
+}
+
+var letterArray = function(sentence){
+  var myArray = wordArray(sentence);
+  // console.log(myArray);
+  var outArray = [];
+  myArray.forEach(function(word) {
+    myArray = [word.split("")];
+    outArray.push(myArray);
+  });
+  console.log(outArray);
+  return outArray;
+}
 
 
 // Front End Logic
@@ -11,6 +28,8 @@ $(document).ready(function(event) {
   $("#pig-form").submit(function(event){
     event.preventDefault();
     var pigInput = $("#pig-input").val();
+
+    letterArray(pigInput);
 
   });
 });
